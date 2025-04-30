9a2cdbf9bd2081ce6bc2a0c46465e37212414e07
dimori2425 <15587067.clot@fje.edu>
2025-04-30 17:28:48 +0200

    Solved database connection

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,31 @@
+// app.ts
 import express, { Request, Response, NextFunction } from 'express';
-import dotenv from 'dotenv';
-
-// Cargar variables de .env en process.env
-dotenv.config();
-import '../db/connection'; // Conexión a la DB MariaDB
-
+import '../db/connection'; // La conexión se configura correctamente con dotenv adentro
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware para parsear JSON en las solicitudes
 app.use(express.json());
 
-// Ruta de bienvenida
 app.get('/', (req: Request, res: Response) => {
-  res.json({
-    text: "conectado a la API"
-  });
+  res.json({ text: "conectado a la API" });
 });
 
-// Ruta con parámetro
 app.get('/api/saludo/:q', (req: Request, res: Response) => {
   const nombre = req.params.q;
   res.json({ mensaje: `Hola, ${nombre}` });
 });
 
-// Ruta POST
 app.post('/api/saludo', (req: Request, res: Response) => {
   const { nombre } = req.body;
   res.status(201).json({ mensaje: `Hola, ${nombre}!` });
 });
 
-// Middleware para manejar errores
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Algo salió mal' });
 });
 
-// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
