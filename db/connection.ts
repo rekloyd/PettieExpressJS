26e105fcfcab9be0cd8547fefdf34075668025cb
rekloyd <paumorillasrivera129@gmail.com>
2025-05-05 20:05:19 +0200

    changed connection to promises

--- db/connection.ts
+++ db/connection.ts
@@ -1,22 +1,24 @@
 import dotenv from 'dotenv';
 dotenv.config(); 
 
-import mysql, { Connection } from 'mysql2';
+import mysql from 'mysql2/promise';
 
-const connection: Connection = mysql.createConnection({
-  host:     process.env.DB_HOST,
-  user:     process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port:     Number(process.env.DB_PORT)
-});
+async function connectDB() {
+  try {
+    const connection = await mysql.createConnection({
+      host:     process.env.DB_HOST,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port:     Number(process.env.DB_PORT),
+    });
 
-connection.connect((err) => {
-  if (err) {
+    console.log('Conectado a MariaDB exitosamente!');
+    return connection;
+  } catch (err) {
     console.error('Error al conectar a MariaDB:', err);
     process.exit(1);
   }
-  console.log('Conectado a MariaDB exitosamente!');
-});
+}
 
-export default connection;
+export default connectDB;
