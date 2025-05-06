fe5ea627b31c59edb60fbca325e6283a400e7e40
rekloyd <paumorillasrivera129@gmail.com>
2025-05-06 17:09:21 +0200

    created an example of db select

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db/connection';
 import { crearUsuariosPorDefecto } from './utils/crearUsuariosPorDefecto';
+import { Connection } from 'mysql2/promise';
 
 dotenv.config();
 
@@ -18,6 +19,17 @@ app.get('/', (req: Request, res: Response) => {
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Algo salió mal' });
+});
+
+app.get('/api/usuarios', async (req: Request, res: Response) => {
+  try {
+    const db = await connectDB();
+    const [rows] = await db.query('SELECT * FROM Usuario');
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Algo salió mal al obtener los usuarios' });
+  }
 });
 
 app.listen(PORT, async () => {
