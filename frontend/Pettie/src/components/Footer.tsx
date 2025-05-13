fd200ea1c4e13c45d0eee990b83d47a02383fc56
rekloyd <paumorillasrivera129@gmail.com>
2025-05-13 17:35:08 +0200

    linked inter font

--- frontend/Pettie/src/components/Footer.tsx
+++ frontend/Pettie/src/components/Footer.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 
 const Footer: React.FC = () => {
+  useEffect(() => {
+    const link = document.createElement('link');
+    link.href =
+      'https://fonts.googleapis.com/css2?family=Inter:wght@400;500&family=Madimi+One&display=swap';
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
+  }, []);
   return (
-    <footer style={{ backgroundColor: '#5D3D12' }} className="text-light py-5 px-4">
+    <footer style={{ backgroundColor: '#5D3D12',fontFamily:"Inter,sans-serif" }} className="text-light py-5 px-4">
       <div className="container">
         <div className="d-flex flex-column flex-lg-row justify-content-between gap-4">
           {/* Primera lista */}
