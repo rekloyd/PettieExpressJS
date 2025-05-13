abbdbd152db9ec423506f34ffa5fe7de22be6710
rekloyd <paumorillasrivera129@gmail.com>
2025-05-13 17:30:59 +0200

    created footer

--- frontend/Pettie/src/App.tsx
+++ frontend/Pettie/src/App.tsx
@@ -2,16 +2,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/" element={<Navbar/>}/>
-      </Routes>
-    </BrowserRouter>
+    <Footer/>
   );
 }
 
---
+++ frontend/Pettie/src/components/Footer.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+
+const Footer: React.FC = () => {
+  return (
+    <footer style={{ backgroundColor: '#5D3D12' }} className="text-light py-5 px-4">
+      <div className="container">
+        <div className="d-flex flex-column flex-lg-row justify-content-between gap-4">
+          {/* Primera lista */}
+          <div>
+            <h5>Enlaces útiles</h5>
+            <ul className="list-unstyled">
+              <li><a href="#" className="text-light text-decoration-none">Inicio</a></li>
+              <li><a href="#" className="text-light text-decoration-none">Servicios</a></li>
+              <li><a href="#" className="text-light text-decoration-none">Blog</a></li>
+              <li><a href="#" className="text-light text-decoration-none">Contacto</a></li>
+            </ul>
+          </div>
+
+          {/* Segunda lista */}
+          <div>
+            <h5>Legal</h5>
+            <ul className="list-unstyled">
+              <li><a href="#" className="text-light text-decoration-none">Aviso legal</a></li>
+              <li><a href="#" className="text-light text-decoration-none">Privacidad</a></li>
+              <li><a href="#" className="text-light text-decoration-none">Cookies</a></li>
+            </ul>
+          </div>
+
+          {/* Formulario */}
+          <div style={{ minWidth: '250px' }}>
+            <h5>Suscríbete a nuestra newsletter</h5>
+            <form>
+              <div className="mb-2">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="newsletterEmail"
+                  placeholder="Tu correo electrónico"
+                  required
+                />
+              </div>
+              <button type="submit" className="btn btn-light text-dark">
+                Suscribirse
+              </button>
+            </form>
+          </div>
+        </div>
+
+        <div className="text-center mt-4 small">
+          &copy; {new Date().getFullYear()} Pettie. Todos los derechos reservados.
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
